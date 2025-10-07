ae4695bb8d4462cab9e8e2685cfb46b62b0d2dd1
Kristy Fountain <kristy.renee.fountain@gmail.com>
2025-10-07 04:38:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3233859570-d7bfa",
+  "appId": "1:697216114097:web:8aed0f6229bfbc55b97190",
+  "apiKey": "AIzaSyD1gC-Mic3GQ1p3fPd3vLuYmO78dgv_vxo",
+  "authDomain": "studio-3233859570-d7bfa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "697216114097"
+};
