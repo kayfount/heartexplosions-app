a9bd7769d3ff8270bffbc7ded508875a37b88e56
Kristy Fountain <kristy.renee.fountain@gmail.com>
2025-10-21 15:41:06 +0000

    **Instinctual-Stacking paired with dominant type.json** → Shows how ea

---
+++ src/lib/instinctual-stacking-paired.ts
@@ -0,0 +1,9 @@
+
+import data from './instinctual-stacking-paired.json';
+
+export interface InstinctualStackingPaired {
+  "Instinctual Stacking paired with Dominant Type": string;
+  "Description": string;
+}
+
+export const InstinctualStackingPairedData: InstinctualStackingPaired[] = data;
